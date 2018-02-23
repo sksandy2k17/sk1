34eeace10ea8724ea438d7942e85a441d435be71
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 21:58:10 +0530

    Update skemail.py

--- skemail.py
+++ skemail.py
@@ -1,14 +1,14 @@
-import smtplib
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
+import smtplib                                      #importing smtp from library
+server = smtplib.SMTP('smtp.gmail.com', 587)        #to access gmail 
+server.starttls()                                   # to start gmail with security
 
-server.login("santhosh.17cs@cmr.edu.in", "8*****"*)
-
-
-
-#Send the mail
-msg = "Hello!"
+server.login("santhosh.17cs@cmr.edu.in", "8*****"*) #login to account
 
 
-server.sendmail("santhosh.17cs@cmr.edu.in", "srrajesh113@gmail.com", msg)
+
+#Send the mail 
+msg = "Hello!"                                      #message
+
+
+server.sendmail("santhosh.17cs@cmr.edu.in", "srrajesh113@gmail.com", msg)    #to send mail
 
