8afdd7f37b5022942b4151a5bf1c02a02b309457
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 22:00:58 +0530

    Update skfunction.py

--- skfunction.py
+++ skfunction.py
@@ -1,13 +1,13 @@
-def add(x,y):
-    return x+y
-def sub(x,y):
-    return x-y
+def add(x,y):                  #funtion definition
+    return x+y                 #returns value if the function is called
+def sub(x,y):       
+    return x-y 
 def mul(x,y):
     return x*y
-a=int(input("enter the value"))
+a=int(input("enter the value"))  #assings the value
 b=int(input("enter the value"))
-print("add of 2nos is",add(a,b))
+print("add of 2nos is",add(a,b))  #calls the funtions and to print
 print("sub of 2 nos",sub(a,b))
 print("mul of 2 nos",mul(a,b))
-print("minimum value u entered",min(a,b))
+print("minimum value u entered",min(a,b))    
 print("maximum value you entere",max(a,b))
