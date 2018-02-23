4290de5e5d9c2e6f98c446ef2acd3908be3447af
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 21:54:50 +0530

    Update skelseif.py

--- skelseif.py
+++ skelseif.py
@@ -1,5 +1,5 @@
-marks=int(input("enter the marks obtained:"))
-if marks>90:
+marks=int(input("enter the marks obtained:"))   #aacpets the mark and converts to integer
+if marks>90:                                    #start of else-if loop
     print("garde A")
 elif marks>80:
     print("grade B")
