b4d2f50d0c22d6ad6afbf64ed3b50263811aff4f
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 22:16:17 +0530

    Update skr,p,s.py

--- skr,p,s.py
+++ skr,p,s.py
@@ -1,17 +1,17 @@
-a=["rock","paper","scissor"]
-a[0]=='r'=='R'
+a=["rock","paper","scissor"]        #list of 'a' 
+a[0]=='r'=='R'                      
 a[1]=='p'=='P'
 a[2]=='s'=='S'
-T=input("enter your choice:")
-import random
-r=random.choice(a)
-print("computer's choice:",r)
-if r==T:
+T=input("enter your choice:")       #to accept the user choice
+import random                       #to import random package
+r=random.choice(a)                  #to pick random choice from list 'a'
+print("computer's choice:",r)       #to print random choice
+if (r==T):                          #start of else-if loop and checks conditions
     print("tie")
-if ("r==rock","T==scissor"):
+elif (r==a[0],T==a[2]):
     print("computer won")
-if("r==scissor","T==paper"):
+elif(r==a[2],T==a[1]):
     print("computer won")
-if ("r==paper","T==rock"):
+elif (r==a[1],T==a[0]):
     print("computer won")
 else: print("you won")
