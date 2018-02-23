ef869f2228ad510cd662bbc971aebff82fcf85e5
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 21:35:34 +0530

    Update sk_multipl_asigmnt.py

--- sk_multipl_asigmnt.py
+++ sk_multipl_asigmnt.py
@@ -1,8 +1,16 @@
-a=int(input("enter the value"))
-import random
-b=random.randint(1,10)
-print("b=",b)
-if a>b:
-    print("a is greather than b")
+a=int(input("enter the value"))   #accepting value for variable a
+import random                     #importing random package
+b=random.randint(1,10)            #assining vale for variable b through random package
+c=int(input("enter the value"))   #accepting value for c
+print("a=",a)                     #to print a
+print("b=",b)                     # to print b
+print("c=",c)                     # to print c
+d,e,f=a,b,c                       #multiple assigment
+if d>e:
+    if d>f:
+        print(d,"is greater than",e,"and",f)
+if e>d:
+    if e>f:
+        print(e,"is greater than",d,"and",f)
 else:
-    print("b is greater than a")
+     print(f,"is greater than",d,"and",f)
