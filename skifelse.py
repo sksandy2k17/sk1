ba27b5fcf8706560bff414a864dc15a33e497586
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 22:04:28 +0530

    Update skifelse.py

--- skifelse.py
+++ skifelse.py
@@ -1,5 +1,5 @@
-a=int(input("enter the value of a:"))
-if a>0:
+a=int(input("enter the value of a:"))   #accepts the value
+if a>0:                                 #start of ifelse loop 
     print(a,"is positive")
 else:
     print(a,"is negavtive")
