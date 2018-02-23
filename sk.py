63a1a062495301f0b53f2fedf55926b8dde2a3ab
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 21:19:00 +0530

    Update sk.py

--- sk.py
+++ sk.py
@@ -1,12 +1,12 @@
-print('santhosh')
-#toprintstring
-a=float(input("enter the value of A"))
+print('santhosh')    #toprintstring
+a=float(input("enter the value of A"))   #to accept input and converts to float 
 b=float(input("enter the value of B"))
-c=a+b
-print(c)
- c=a-b
+c=a+b    #to add
+print(c) #to print
+ c=a-b    # to subtract
+ print(c)  
+ c=a*b    # to multiply
  print(c)
- c=a*b
+ c=a%b  # to get remainder
  print(c)
- c=a%b
- print(c)
+ 
