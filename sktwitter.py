017690adda1f214541430282632d1e3cd36965b2
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 22:26:27 +0530

    Update sktwitter.py

--- sktwitter.py
+++ sktwitter.py
@@ -1,23 +1,21 @@
-import tweepy
+import tweepy                              #imports tweepy package from loibrary
 
-def get_api(cfg):
+def get_api(cfg):                          #function defintion 
   auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
   auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
   return tweepy.API(auth)
 
 def main():
-  # Fill in the values noted in previous step here
   cfg = { 
-    "consumer_key"        : "PNKaqft17CZJC3mNzNe2ZjOnX",
-    "consumer_secret"     : "wKZU64lF10ZerXmbV8ROMqdVxtaGbKfgB3C43Bn357RJVfQkR2",
-    "access_token"        : "711529082832363520-tWk3ecKd5qLjhL6xBkmsQzOk6ResQIh",
-    "access_token_secret" : "7WEN42IPXEhoKZ6QukPrl6m0M6TaRNlVinDhLIiyt5ccC" 
+    "consumer_key"        : "%%%%%%%%%%%%%%%%%%%%",
+    "consumer_secret"     : "###################",
+    "access_token"        : "$$$$$$$$$$$$$$$$$$$$",
+    "access_token_secret" : "nnnnnnnnnnnnnnnnnnnn" 
     }
 
   api = get_api(cfg)
   tweet = "Hello, world!"
   status = api.update_status(status=tweet) 
-  # Yes, tweet is called 'status' rather confusing
 
 if __name__ == "__main__":
   main()
