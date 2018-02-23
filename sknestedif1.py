73758f632c75a007c1a2c168def08d1261e79a23
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 22:05:58 +0530

    Update sknestedif1.py

--- sknestedif1.py
+++ sknestedif1.py
@@ -1,7 +1,7 @@
-n=7
-if n>3:
+n=7                         #assings the value for n
+if n>3:                     #start of if loop
     print('3')
-    if n<5:
+    if n<5:                 #nested if:if loop inside another if loop
         print('5')
         if n==7:
             print('7')
