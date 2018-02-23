1733f5718b929fc26d6e1df1961f4a05fbf0faaa
sksandy2k17 <35676468+sksandy2k17@users.noreply.github.com>
2018-02-23 22:03:01 +0530

    Update skif.py

--- skif.py
+++ skif.py
@@ -1,5 +1,5 @@
-a=int(input("enter the number to check positive or negative"))
-if a>=0:
+a=int(input("enter the number to check positive or negative"))     #accepts the value
+if a>=0:                                                            #start of if statement and checks condition
     print("given number is positive")
 if a<0:
     print("given number is negative")
